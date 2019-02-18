a295a922e08b9c212670b371d789bdb05d3fd172
Joseph Pratt <josephlars@hotmail.com>
2019-02-19 11:43:28 +1300

    Get user input from command line argument rather than readline

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-//opening msg
-let msg = 'JELP Balance Checker v1.0';
-console.log(msg);
+// grab argument (string to check)
+let input = process.argv[2];
+// check for equality
+let isEqual = checkEqual(input);
+// construct message
+var message = 'User input: "' + input + '" is ';
+if (isEqual) {
+	message += 'balanced.';
+} else {
+	message += 'not balanced.';
+}
+// display result
+console.log(message);
 
-//grab string
-var readline = require('readline');
-var rl = readline.createInterface(process.stdin, process.stdout);
-
-rl.question(`Whats your name?`, function(name) {
-	let isEqual = checkEqual(name)
-	//display result
-	console.log('equality check: ' + isEqual);
-	rl.close();
-});
-
-//checks string for equality
+// checks string for equality
 function checkEqual(input){
-	console.log('Checking "' + input + '" for equality')
 
 	let stack = [];
 	for (const c of input) {
-		console.log(c);
 		if (c == '(') {
 			stack.push(c);
 		} else if (c == ')') {
@@ -31,14 +28,10 @@ function checkEqual(input){
 		}
 	}
 
-	console.log("stack: ");
-	console.log(stack);
-
 	if (stack.length === 0) {
 		return true;
 	} else {
 		return false;
 	}
-
 
 } 
